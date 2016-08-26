a5baa5676405fd035c0d45c3a1d9f1ce039826c3
szhu1026 <szhu1026@gmail.com>
2016-08-26 10:57:30 -0400

    Add video CSS

--- frontend/components/movie_intro_component.jsx
+++ frontend/components/movie_intro_component.jsx
@@ -3,6 +3,8 @@ let MovieStore = require('../stores/movie_store');
 let MovieActions = require('../actions/movie_actions');
 let Link = require('react-router').Link;
 
+// https://www.youtube.com/embed/tntOCGkgt98
+
 let MovieIntroComponent = React.createClass({
   render: function(){
       return (
@@ -11,6 +13,7 @@ let MovieIntroComponent = React.createClass({
           <p className="movie-rating"> {this.props.movie.vote_average}/10</p>
             <p className="movie-title"> {this.props.movie.original_title} </p>
             <img className="movie-image" src={`http://image.tmdb.org/t/p/w154/${this.props.movie.poster_path}`}/>
+            <iframe className="trailer" width="500" height="225" src="https://www.youtube.com/watch_popup?v=tntOCGkgt98" frameborder="0" allowfullscreen></iframe>
           </div>
           <div className="movie-intro-text">
             <p className="movie-biography"> {this.props.movie.overview} </p>
